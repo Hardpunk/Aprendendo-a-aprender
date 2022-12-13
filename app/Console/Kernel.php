a89817537e6b3566b1f0916f9622ed0c6c5087e7
Bruno Spoladore <brunospoladore@hotmail.com>
2022-12-13 09:38:09 -0300

    Added update courses cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,18 +13,21 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\AtualizaCursosCron::class
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // Atualizar categorias e cursos às 03h00 e às 15h00
+        $schedule->command('atualizacursos:cron')
+            ->timezone('America/Sao_Paulo')
+            ->twiceDaily(3, 15);
     }
 
     /**
@@ -32,7 +35,7 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
